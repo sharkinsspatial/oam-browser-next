448c16be4bda02150c3c3907d43076197a1bc24a
sharkinsspatial <sharkinsgis@gmail.com>
2018-07-31 22:57:00 -0300

    Component test.

---
+++ test/test_Container.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import test from 'tape';
+import { shallow, configure } from 'enzyme';
+import AppBar from '@material-ui/core/AppBar';
+import { Container } from '../src/components/Container';
+
+configure({ adapter: new Adapter() });
+
+const getProps = () => ({
+  classes: {
+    root: '',
+    item: ''
+  }
+});
+
+test('Container', (t) => {
+  const props = getProps();
+  const wrapper = shallow((<Container {...props} />));
+  t.equal(
+    wrapper.find(AppBar).length, 1,
+    'Container has a Toolbar'
+  );
+  t.end();
+});
