6f9799100f94737b74bccfdc4980094c88cc09df
sharkinsspatial <sharkinsgis@gmail.com>
2018-08-02 11:44:58 -0300

    Immutable filtering for stylesheets.

--- src/reducers/stylesheetReducer.js
+++ src/reducers/stylesheetReducer.js
@@ -1,14 +1,43 @@
 import { Map, fromJS } from 'immutable';
 import * as actions from '../constants/action_types';
-//import { MapActionTypes } from '@mapbox/mapbox-gl-redux';
+import * as stylesheetConstants from '../constants/stylesheetConstants';
+
+const filterItems = (state, payload) => {
+  const {
+    clusterLayer,
+    clusterCountLayer,
+    unclusteredPointLayer
+  } = stylesheetConstants;
+
+  const newState = state
+    .updateIn(['style', 'layers'], (list) => {
+      const idx = list.findIndex(layer => layer.get('id') === clusterLayer);
+      return list.setIn([idx, 'filter'], fromJS(payload.clusterFilter));
+    })
+    .updateIn(['style', 'layers'], (list) => {
+      const idx = list.findIndex(layer => layer.get('id') === clusterCountLayer);
+      return list.setIn([idx, 'filter'], fromJS(payload.clusterFilter));
+    })
+    .updateIn(['style', 'layers'], (list) => {
+      const idx = list.findIndex(layer => layer.get('id') === unclusteredPointLayer);
+      return list.setIn([idx, 'filter'], fromJS(payload.pointFilter));
+    });
+
+  return newState;
+};
 
 const initialState = Map({ style: fromJS({}) });
+
 export default function stylesheetReducer(state = initialState, action) {
   switch (action.type) {
     case actions.SET_STYLE: {
       return state.merge({
         style: fromJS(action.payload.style)
       }); }
+
+    case actions.FILTER_ITEMS: {
+      return filterItems(state, action.payload);
+    }
 
     default: {
       return state;
