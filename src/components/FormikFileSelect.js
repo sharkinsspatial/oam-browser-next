71772027d7efa66d3ae4638e7d600009cc267488
sharkinsspatial <sharkinsgis@gmail.com>
2018-11-01 19:03:55 -0300

    Form refactoring for file select and validation.

---
+++ src/components/FormikFileSelect.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getIn } from 'formik';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  input: {
+    display: 'none',
+  },
+  fileNameLabel: {
+    display: 'inline-block',
+    marginLeft: theme.spacing.unit
+  }
+});
+
+const FormikFileSelect = (props) => {
+  const {
+    name,
+    values,
+    setValues,
+    classes
+  } = props;
+  const file = getIn(values, name);
+  return (
+    <React.Fragment>
+      <input
+        name={name}
+        accept="image/*"
+        className={classes.input}
+        id="raised-button-file"
+        type="file"
+        onChange={(event) => {
+          const newValues = { ...values };
+          // eslint-disable-next-line prefer-destructuring
+          newValues[event.currentTarget.name] = event.currentTarget.files[0];
+          setValues(newValues);
+        }}
+      />
+      <label htmlFor="raised-button-file">
+        <Button
+          component="span"
+          variant="outlined"
+        >
+          Select Local File
+        </Button>
+      </label>
+      <Typography className={classes.fileNameLabel}>
+        {file ? file.name : 'None selected'}
+      </Typography>
+    </React.Fragment>
+  );
+};
+
+FormikFileSelect.propTypes = {
+  name: PropTypes.string.isRequired,
+  values: PropTypes.object.isRequired,
+  setValues: PropTypes.func.isRequired,
+  classes: PropTypes.shape({
+    input: PropTypes.string.isRequired,
+    fileNameLabel: PropTypes.string.isRequired
+  }).isRequired,
+};
+export default withStyles(styles)(FormikFileSelect);
--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -3,21 +3,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withFormik } from 'formik';
+import * as Yup from 'yup';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import { withStyles } from '@material-ui/core/styles';
 import { sendUpload } from '../actions/uploadActions';
 import FormikTextField from './FormikTextField';
+import FormikFileSelect from './FormikFileSelect';
 
 const styles = theme => ({
+  paper: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
   button: {
     margin: theme.spacing.unit,
   },
-  input: {
-    display: 'none',
-  },
   grid: {
     marginTop: '20px'
+  },
+  submit: {
+    textAlign: 'center'
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit
   }
 });
 
@@ -27,6 +39,7 @@ export const UploadForm = (props) => {
     handleSubmit,
     setValues,
     values,
+    isValid,
     ...formikFieldProps
   } = props;
   return (
@@ -36,76 +49,72 @@ export const UploadForm = (props) => {
       className={classes.grid}
     >
       <Grid item xs={8}>
-        <form onSubmit={handleSubmit}>
-          <FormikTextField
-            name="email"
-            type="email"
-            label="Email"
-            values={values}
-            {...formikFieldProps}
-          />
-          <br />
-          <FormikTextField
-            name="password"
-            type="password"
-            label="Password"
-            values={values}
-            {...formikFieldProps}
-          />
-          <br />
-          <br />
-          <input
-            name="file"
-            accept="image/*"
-            className={classes.input}
-            id="raised-button-file"
-            type="file"
-            onChange={(event) => {
-              const newValues = { ...values };
-              // eslint-disable-next-line prefer-destructuring
-              newValues[event.currentTarget.name] = event.currentTarget.files[0];
-              setValues(newValues);
-            }}
-          />
-          <label htmlFor="raised-button-file">
-            <Button
-              component="span"
-              className={classes.button}
-            >
-              Upload
-            </Button>
-          </label>
-          <br />
-          <br />
-          <Button
-            className={classes.button}
-            type="submit"
-          >
-             Submit
-          </Button>
-        </form>
+        <Paper className={classes.paper}>
+          <form onSubmit={handleSubmit}>
+            <FormikTextField
+              name="email"
+              type="email"
+              label="Email"
+              values={values}
+              {...formikFieldProps}
+            />
+            <br />
+            <FormikTextField
+              name="password"
+              type="password"
+              label="Password"
+              values={values}
+              {...formikFieldProps}
+            />
+            <br />
+            <br />
+            <FormikFileSelect
+              name="file"
+              values={values}
+              setValues={setValues}
+            />
+            <br />
+            <br />
+            <div className={classes.submit}>
+              <Button
+                className={classes.button}
+                type="submit"
+                variant="contained"
+                color="primary"
+                disabled={!isValid}
+              >
+                Submit Upload
+                <CloudUploadIcon className={classes.rightIcon} />
+              </Button>
+            </div>
+          </form>
+        </Paper>
       </Grid>
     </Grid>
   );
 };
 
-const validate = (values) => {
-  const errors = {};
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (
-    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-  ) {
-    errors.email = 'Invalid email address';
-  }
-  return errors;
-};
+const SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg'];
+const UploadSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required'),
+  password: Yup.string()
+    .min(4, 'Too Short!')
+    .required('Required'),
+  file: Yup.mixed()
+    .required('A file is required')
+    .test(
+      'fileFormat',
+      'Unsupported Format',
+      value => value && SUPPORTED_FORMATS.includes(value.type)
+    )
+});
 
 const EnhancedUploadForm = withFormik({
   mapPropsToValues: () => ({ email: '', password: '' }),
 
-  // Custom sync validation
-  validate,
+  validationSchema: UploadSchema,
 
   handleSubmit: (values, { props, setSubmitting }) => {
     const { sendUpload: sendUploadAction } = props;
@@ -121,12 +130,12 @@ const EnhancedUploadForm = withFormik({
 UploadForm.propTypes = {
   classes: PropTypes.shape({
     button: PropTypes.string.isRequired,
-    input: PropTypes.string.isRequired,
     grid: PropTypes.string.isRequired
   }).isRequired,
   handleSubmit: PropTypes.func.isRequired,
   setValues: PropTypes.func.isRequired,
-  values: PropTypes.object.isRequired
+  values: PropTypes.object.isRequired,
+  isValid: PropTypes.bool.isRequired
 };
 
 const mapDispatchToProps = { sendUpload };
