94d29111d46cd75babac86c919a756db2b4c32d9
sharkinsspatial <sharkinsgis@gmail.com>
2018-11-07 13:07:52 -0300

    Refactor apiMiddleware to use async/await. Closes #4.

--- src/store/apiMiddleware.js
+++ src/store/apiMiddleware.js
@@ -1,37 +1,9 @@
-/* global fetch */
 import { getToken } from '../utils/tokens';
 import { setHasValidToken } from '../actions/authActions';
 import { CALL_API } from '../constants/action_types';
+import fetchWrapper from '../utils/fetchWrapper';
 
-const BASE_URL = process.env.REACT_APP_API_URL;
-
-function callApi(endpoint, method, token, json) {
-  const config = {
-    method,
-    Accept: 'application/json',
-    headers: { 'content-type': 'application/json' },
-    mode: 'cors',
-    body: JSON.stringify(json)
-  };
-
-  if (token) {
-    config.headers = { Authorization: token };
-  }
-
-  return fetch(BASE_URL + endpoint, config)
-    .then(response => response.text()
-      .then(text => ({ text, response })))
-    .then(({ text, response }) => {
-      if (!response.ok) {
-        const error = new Error(response.statusText);
-        return Promise.reject(error);
-      }
-      return text;
-    })
-    .catch(err => Promise.reject(err.message));
-}
-
-const apiMiddleware = store => next => (action) => {
+const apiMiddleware = store => next => async (action) => {
   if (action.type !== CALL_API) {
     return next(action);
   }
@@ -44,39 +16,34 @@ const apiMiddleware = store => next => (action) => {
   } = action.payload;
 
   const { requestType, successType, errorType } = types;
-
-  const handleResponse = (response) => {
-    const responseJSON = JSON.parse(response);
+  store.dispatch({ type: requestType });
+  let token;
+  if (authenticated) {
+    token = getToken();
+    if (!token) {
+      store.dispatch(setHasValidToken(false));
+    }
+  }
+  try {
+    const responseJSON = await fetchWrapper(endpoint, method, token, json)
+      .catch(error => (
+        next({
+          error: error.message || 'There was an error.',
+          type: errorType
+        })
+      ));
     return next({
       payload: {
         json: responseJSON,
-        authenticated,
       },
       type: successType
     });
-  };
-
-  const handleErrorResponse = error => next({
-    error: error.message || 'There was an error.',
-    type: errorType
-  });
-
-  store.dispatch({ type: requestType });
-  let call;
-  if (authenticated) {
-    const token = getToken();
-    if (!token) {
-      store.dispatch(setHasValidToken(false));
-    } else {
-      call = callApi(endpoint, method, token, json)
-        .then(handleResponse)
-        .catch(handleErrorResponse);
-    }
-  } else {
-    call = callApi(endpoint, method, null, json)
-      .then(handleResponse)
-      .catch(handleErrorResponse);
+  } catch (error) {
+    return next({
+      error: error.message || 'There was an error.',
+      type: errorType
+    });
   }
-  return call;
 };
+
 export default apiMiddleware;
