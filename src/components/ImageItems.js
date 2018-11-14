bc3794ff3c7608a8e54c1a21ed3d1df09c680846
sharkinsspatial <sharkinsgis@gmail.com>
2018-11-14 10:41:25 -0300

    Include text block in sidebar.

--- src/components/ImageItems.js
+++ src/components/ImageItems.js
@@ -5,18 +5,28 @@ import { connect } from 'react-redux';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import ListSubheader from '@material-ui/core/ListSubheader';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 import { push } from 'redux-little-router';
 import { setActiveImageItem } from '../actions/stylesheetActionCreators';
 import * as stylesheetSelectors from '../reducers/stylesheetSelectors';
 import ImageItem from './ImageItem';
 
+const styles = theme => ({
+  instructions: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit
+  }
+});
 export const ImageItems = (props) => {
   const {
     imageItems,
     push: dispatchPush,
     setActiveImageItem: dispatchSetActiveImageItem,
-    activeImageItemId
+    activeImageItemId,
+    classes
   } = props;
+
   const items = imageItems.map((item) => {
     const id = item.getIn(['properties', 'id']);
     const thumbnail = item.getIn(['assets', 'thumbnail', 'href']);
@@ -47,7 +57,18 @@ export const ImageItems = (props) => {
     >
       <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
         <ListSubheader component="div">
-          OAM
+          <Typography variant="h6">
+            OpenAerialMap (OAM) is a set of tools for searching,
+            sharing, and using openly licensed satellite and unmanned aerial
+            vehicle (UAV) imagery.
+          </Typography>
+          <Typography
+            variant="body1"
+            className={classes.instructions}
+          >
+            To get started click on a cluster to view the images it contains.
+            If the cluster is red, zoom in until it splits into smaller clusters.
+          </Typography>
         </ListSubheader>
       </GridListTile>
       {items}
@@ -59,7 +80,8 @@ ImageItems.propTypes = {
   imageItems: ImmutablePropTypes.list.isRequired,
   push: PropTypes.func.isRequired,
   setActiveImageItem: PropTypes.func.isRequired,
-  activeImageItemId: PropTypes.number.isRequired
+  activeImageItemId: PropTypes.number.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -69,4 +91,4 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = { push, setActiveImageItem };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ImageItems);
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(ImageItems));
